7eb001cbf537b3fcf5701aef125f6b06bad56ed9
yogg-soggot <nikit-iv777@ya.ru>
2019-10-25 21:20:31 +0300

    all works properly may be need some minor changes

--- app/src/main/java/com/yoggsoggot/testapplication/ui/PostAdapter.kt
+++ app/src/main/java/com/yoggsoggot/testapplication/ui/PostAdapter.kt
@@ -1,4 +1,36 @@
 package com.yoggsoggot.testapplication.ui
 
-class PostAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.yoggsoggot.testapplication.R
+
+class PostAdapter  internal constructor(context: Context): RecyclerView.Adapter<PostAdapter.PostViewHolder>(){
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var posts = emptyList<String>()
+
+    inner class PostViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val postItemView: TextView = itemView.findViewById(R.id.post)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_post_item,parent,false)
+        return PostViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val current = posts[position]
+        holder.postItemView.text = current
+    }
+
+    internal fun setPosts(posts: List<String>){
+        this.posts = posts
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = posts.size
+
 }
--- app/src/main/java/com/yoggsoggot/testapplication/ui/UserActivity.kt
+++ app/src/main/java/com/yoggsoggot/testapplication/ui/UserActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.yoggsoggot.testapplication.R
@@ -18,6 +19,12 @@ class UserActivity: AppCompatActivity() {
         setContentView(R.layout.activity_user)
 
         val userId = intent.getIntExtra("userId",0)
+
+        val adapter = PostAdapter(this)
+
+        postRecyclerView.adapter = adapter
+        postRecyclerView.layoutManager = LinearLayoutManager(this)
+
 
 
 
@@ -37,6 +44,8 @@ class UserActivity: AppCompatActivity() {
                 .into(user_image_view)
 
             val posts = toList(user.posts)
+
+            adapter.setPosts(posts!!)
         })
 
 
